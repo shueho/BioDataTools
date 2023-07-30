af9af78c923df438a229c0479e83e8c180b11d8a
shueho <66934820+shueho@users.noreply.github.com>
2023-07-30 13:35:19 +0800

    Update and rename get_sum.py to Metagenome/get_sum.py

--- Metagenome/get_sum.py
+++ Metagenome/get_sum.py
@@ -1,6 +1,15 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+#
+# @Version : 1.0
+# @Time    : 2023/7/30 13:00
+# @Author  : Hao Xue
+# @E-mail  : studid@163.com
+# @File    : get_sum.py
+#
+# Place the script in the parent directory of the assembly evaluation results completed by quast software to obtain the assembly evaluation information summary table.
+
 import os
-
-
 print("Author: XueHao\nE-mail: studid@163.com")
 print("begin")
 ls = list(os.popen("find -name transposed_report.tsv"))
