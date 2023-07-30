f6ef0a055be1b6729f44aca8e896a4a307135bf1
shueho <66934820+shueho@users.noreply.github.com>
2023-07-30 13:33:02 +0800

    Update and rename fasta_rename.py to Metagenome/fasta_rename.py

--- Metagenome/fasta_rename.py
+++ Metagenome/fasta_rename.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+#
+# @Version : 1.0
+# @Time    : 2023/7/30 13:00
+# @Author  : Hao Xue
+# @E-mail  : studid@163.com
+# @File    : fasta_rename.py
+#
+# Modify the name in each sequence of the specified FASTA file to make it standardized and uniform. 
 
 import sys 
 import os
