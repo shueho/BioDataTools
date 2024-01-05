a6af10d394bbfc8f1d544b98151ad01929864a73
shueho <66934820+shueho@users.noreply.github.com>
2024-01-05 15:28:19 +0800

    Create GenoSpider.py

---
+++ Gadget/GenoSpider/GenoSpider.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+#
+# @Version : 1.0
+# @Time    : 2024/1/4 16:00
+# @Author  : Hao Xue
+# @E-mail  : studid@163.com
+# @File    : GenoSpider.py
+#
+# Genome Information Crawler tool.
